4572abab2c1ab483311dc89e5944b939a3dd4b6d
guesswhojustme <imnayeonsaranghaee@gmail.com>
2025-10-29 15:07:44 +0800

    feat: add 50% JavaScript base logic for library app

---
+++ script.js
@@ -0,0 +1,39 @@
+const bookID = crypto.randomUUID();
+
+function Book(title, author, page, id) {
+  this.title = title;
+  this.autohor = author; 
+  this.page = page;
+  this.id = id;
+}
+
+Book.prototype.showBookInfo = function () {
+  return `Title: ${this.title}, Author: ${this.autohor}, Pages: ${this.page}`;
+}
+
+const defaultBook = new Book('The Chronicles of Narnia', 'C.S. Lewis', '768', bookID);
+
+function addBookToLibrary(title, author, page){
+    const addedBook = new Book(title, author, page, bookID)
+ 
+    return addedBook;
+}
+
+const myLibrary = [defaultBook];
+
+function displayBooks(library){
+  for(i = 0; i < library.length; i++){
+    library[i]
+  }
+  return library;
+}
+
+myLibrary.push(addBookToLibrary('Test', 'Test', 123))
+myLibrary.push(addBookToLibrary('Test1', 'Test1', 1234))
+
+
+console.log(displayBooks(myLibrary));
+
+
+
+
