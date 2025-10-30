42e318a38ebc18323779f8797ab5a74d0fb9494a
guesswhojustme <imnayeonsaranghaee@gmail.com>
2025-10-30 21:47:52 +0800

    add new book function display on the page

--- script.js
+++ script.js
@@ -1,23 +1,38 @@
 const title = document.getElementById('title');
 const author = document.getElementById('author');
 const pages = document.getElementById('page');
-const addBookBtn = document.getElementById('add-book')
-
+const addBookBtn = document.getElementById('add-book');
+const bookDiv = document.getElementById('book-div');
+const bookContainer = document.querySelector('.books-container')
 
 const bookID = crypto.randomUUID();
 
 function Book(title, author, page, id) {
+  if (!new.target) {
+    throw Error("You must use the 'new' operator to call the constructor");
+  }
   this.title = title;
-  this.autohor = author; 
+  this.author = author; 
   this.page = page;
   this.id = id;
 }
 
 Book.prototype.showBookInfo = function () {
-  return `Title: ${this.title}, Author: ${this.autohor}, Pages: ${this.page}`;
+  return `Title: ${this.title}, Author: ${this.author}, Pages: ${this.page}`;
 }
 
 const defaultBook = new Book('The Chronicles of Narnia', 'C.S. Lewis', '768', bookID);
+
+const defaultBookAuthor = document.createElement('span');
+defaultBookAuthor.textContent = defaultBook.author;
+const defaultBookTitle = document.createElement('span')
+defaultBookTitle.textContent = defaultBook.title;
+const defaultBookPage = document.createElement('span')
+defaultBookPage.textContent = defaultBook.page;
+
+bookDiv.appendChild(defaultBookAuthor)
+bookDiv.appendChild(defaultBookTitle)
+bookDiv.appendChild(defaultBookPage)
 
 function addBookToLibrary(title, author, page){
     const addedBook = new Book(title, author, page, bookID)
@@ -34,15 +49,34 @@ function displayBooks(library){
   return library;
 }
 
-myLibrary.push(addBookToLibrary('Test', 'Test', 123))
-myLibrary.push(addBookToLibrary('Test1', 'Test1', 1234))
-
 console.log(displayBooks(myLibrary));
+console.log(defaultBook.author);
+
 
 addBookBtn.addEventListener('click', () => {
   myLibrary.push(addBookToLibrary(title.value, author.value, pages.value, bookID))
   console.log(addBookToLibrary(title.value, author.value, pages.value, bookID));
   console.log(displayBooks(myLibrary));
+
+  const newDiv = document.createElement('div');
+  newDiv.style.paddingTop = "20px"
+  bookContainer.appendChild(newDiv);
+
+  const newBookDiv = document.createElement('div');
+  newBookDiv.id = "book-div";
+  newDiv.appendChild(newBookDiv);
+
+  const newBookAuthor = document.createElement('span');
+  newBookAuthor.textContent = author.value;
+  const newBookTitle = document.createElement('span');
+  newBookTitle.textContent = title.value;
+  const newBookPage = document.createElement('span');
+  newBookPage.textContent = pages.value;
+
+  newBookDiv.appendChild(newBookAuthor);
+  newBookDiv.appendChild(newBookTitle);
+  newBookDiv.appendChild(newBookPage);
+
 })
 
 
